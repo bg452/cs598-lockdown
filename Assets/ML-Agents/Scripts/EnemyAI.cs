4150f63b2872f6cfda5b16bb378f265a7ea76a1f
Andrew Hernandez <andrewh717@gmail.com>
2018-10-04 20:50:25 -0400

    Changed Enemy so that it chases Civilians
    Disabled Patrol.cs for the Enemy object and added EnemyAI.cs to it instead

---
+++ Assets/ML-Agents/Scripts/EnemyAI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour {
+	public Transform Civilian1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<NavMeshAgent>().destination = Civilian1.position;
+	}
+}
