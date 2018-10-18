20449a6758f7f56df96be75177cae653614324d7
bg452 <bg452@scarletmail.rutgers.edu>
2018-10-17 23:27:41 -0400

    Hostile agent moves toward the closest civilian.

--- Assets/ML-Agents/Scripts/EnemyAI.cs
+++ Assets/ML-Agents/Scripts/EnemyAI.cs
@@ -4,14 +4,37 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour {
-	public Transform Civilian1;
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private GameObject[] allCivilians;
+
+    // Use this for initialization
+    void Start () {
+        allCivilians = GameObject.FindGameObjectsWithTag("Civilian");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		GetComponent<NavMeshAgent>().destination = Civilian1.position;
+
+		GetComponent<NavMeshAgent>().destination = findClosestObj(allCivilians).transform.position;
 	}
+
+
+    GameObject findClosestObj(GameObject[] targetObject)
+    {
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+
+        foreach (GameObject obj in targetObject)
+        {
+            Vector3 diff = obj.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = obj;
+                distance = curDistance;
+            }
+        }
+
+        return closest;
+    }
 }
