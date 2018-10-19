74d888a0002e6030d7e860415ad730e523e46cf7
bg452 <bg452@scarletmail.rutgers.edu>
2018-10-18 22:31:32 -0400

    Civilians move to safety zone when they get close to a hostile agent

--- Assets/lockdown/moveToSafetyZone.cs
+++ Assets/lockdown/moveToSafetyZone.cs
@@ -6,35 +6,38 @@ using UnityEngine.AI;
 public class moveToSafetyZone : MonoBehaviour {
     private NavMeshAgent agent;
     private GameObject[] allSafeZones;
+    private GameObject[] allEnemies;
 
+    private float minDistToEnemy = 20f;
 
-    public float moveSpeed = 3f;
+    public float moveSpeed = 2.5f;
     public float rotSpeed = 10f;
 
     private bool isRotaingLeft = false;
     private bool isRoatingRight = false;
     private bool isWalking = false;
 
-
-
     private bool isWander = false;
-    private bool found = false;
+
     // Use this for initialization
     void Start(){
         agent = GetComponent<NavMeshAgent>();
         allSafeZones = GameObject.FindGameObjectsWithTag("Safety");
+        allEnemies = GameObject.FindGameObjectsWithTag("Enemy");
     }
 
 	// Update is called once per frame
 	void Update () {
 
-        if (!found)
+        if (seenEnemy())
         {
-            if (!isWander)
-            {
-                StartCoroutine(Wander());
+            agent.destination = findClosestObj(allSafeZones).transform.position;
+            agent.speed = 3.5f;
+        }
+        else
+        {
+            StartCoroutine(Wander());
 
-            }
             if (isRoatingRight)
             {
                 transform.Rotate(transform.up * Time.deltaTime * rotSpeed);
@@ -49,31 +52,7 @@ public class moveToSafetyZone : MonoBehaviour {
             {
                 transform.position += transform.forward * moveSpeed * Time.deltaTime;
             }
-            safeDistance(allSafeZones);
         }
-        else{
-            agent.destination = findClosestObj(allSafeZones).transform.position;
-        }
-    }
-
-    void safeDistance(GameObject[] targetObject)
-    {
-
-        GameObject closest = null;
-        float distance = Mathf.Infinity;
-        Vector3 position = transform.position;
-
-        foreach (GameObject obj in targetObject)
-        {
-            Vector3 diff = obj.transform.position - position;
-            float curDistance = diff.sqrMagnitude;
-            if (curDistance < 200f)
-            {
-                found = true;
-            }
-        }
-
-
     }
 
     IEnumerator Wander()
@@ -89,25 +68,32 @@ public class moveToSafetyZone : MonoBehaviour {
         yield return new WaitForSeconds(walkTime);
         isWalking = false;
         yield return new WaitForSeconds(rotWait);
+
         if (rotateLorR == 1)
         {
-
             isRoatingRight = true;
             yield return new WaitForSeconds(rotTime);
             isRotaingLeft = false;
-
         }
         if (rotateLorR == 2)
         {
-
             isRotaingLeft = true;
             yield return new WaitForSeconds(rotTime);
             isRoatingRight = false;
-
         }
-        isWander = false;
+    }
 
+    bool seenEnemy()
+    {
+        bool retVal = false;
+        GameObject closestEnemy = findClosestObj(allEnemies);
+        float currDistToEnemy = (closestEnemy.transform.position - agent.transform.position).sqrMagnitude;
+        if (currDistToEnemy <= minDistToEnemy)
+        {
+            retVal = true;
+        }
 
+        return retVal;
     }
 
     GameObject findClosestObj(GameObject[] targetObject)
