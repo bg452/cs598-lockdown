a14e0bc44a743efdc05827bdd1de6a4cd39b15c1
bg452 <bg452@scarletmail.rutgers.edu>
2018-11-08 12:04:48 -0500

    Updated initial placement of agents so that civilians and enemies are not too close to each other in the beginning.

--- Assets/Scripts/initAgents.cs
+++ Assets/Scripts/initAgents.cs
@@ -10,30 +10,32 @@ public class initAgents : MonoBehaviour
     private int numEnemy = 2;
     private int numCivilians = 12;
 
-    private float x;
-    private float y = 1;
-    private float z;
-    private Vector3 pos;
+    private Vector3[] enemy_pos = new[] { new Vector3(14, 1, -3), new Vector3(57, 1, -22),
+        new Vector3(44, 1, 22), new Vector3(27, 1, 5) };
 
     // Use this for initialization
     void Start()
     {
+        // different initial position for enemies, works for 2. Can use a list for > 2
+        int prev_ind = Random.Range(0, enemy_pos.Length - 1);
+
         for (int i = 0; i < numEnemy; ++i)
         {
-            x = Random.Range(11, 60);
-            z = Random.Range(-24, 24);
-            pos = new Vector3(x, y, z);
+            int rand_ind = Random.Range(0, enemy_pos.Length - 1);
+            while (rand_ind == prev_ind)
+            {
+                rand_ind = Random.Range(0, enemy_pos.Length - 1);
+            }
 
-            GameObject enemy = Instantiate(enemyPrefab, pos, Quaternion.identity);
+            GameObject enemy = Instantiate(enemyPrefab, enemy_pos[rand_ind], Quaternion.identity);
             enemy.transform.SetParent(GameObject.Find("Enemies").transform);
+            prev_ind = rand_ind;
         }
 
         for (int i = 0; i < numCivilians; ++i)
         {
-            x = Random.Range(11, 60);
-            z = Random.Range(-24, 24);
-            pos = new Vector3(x, y, z);
-
+            Vector3 pos = new Vector3(Random.Range(11, 60), 1, Random.Range(-24, 24));
+            pos = checkDistToEnemy(pos);
             GameObject civilian = Instantiate(civilianPrefab, pos, Quaternion.identity);
             civilian.transform.SetParent(GameObject.Find("Civilians").transform);
         }
@@ -42,5 +44,25 @@ public class initAgents : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    }
+
+    Vector3 checkDistToEnemy(Vector3 pos)
+    {
+        Vector3 retVal = pos;
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        float minDist = 12f;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float dist = (enemy.transform.position - pos).sqrMagnitude;
+            if (dist <= minDist)
+            {
+                retVal.x += minDist;
+                retVal.z = -1 * retVal.z;
+                break;
+            }
+        }
+
+        return retVal;
     }
 }
