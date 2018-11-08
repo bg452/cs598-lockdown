065cb03844ddbc1761f08b34b249c9a8b0d51756
bg452 <bg452@scarletmail.rutgers.edu>
2018-11-08 14:42:11 -0500

    Added start button

--- Assets/Scripts/gameController.cs
+++ Assets/Scripts/gameController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class gameController : MonoBehaviour {
     public GameObject gameOverPanel;
@@ -27,5 +28,11 @@ public class gameController : MonoBehaviour {
             GameObject.Find("GameOverText").GetComponent<Text>().text = "You win!";
         }
         EnemyAI.playerWin = false;
+    }
+
+    public void RestartGame()
+    {
+        gameOverPanel.SetActive(false);
+        SceneManager.LoadScene("lockdown");
     }
 }
---
+++ Assets/Scripts/startScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startScene : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        GameObject.Find("StartGameButton").SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void startGame()
+    {
+        GameObject.Find("StartGameButton").SetActive(false);
+        SceneManager.LoadScene("lockdown");
+    }
+}
